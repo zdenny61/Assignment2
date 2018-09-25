cf66756dc6edc9b3c6a332178af6a0bd1090da6b
zdenny61 <zdenny61@outlook.com>
2018-09-25 19:38:33 -0400

    Create BearSong.c
    Added methods for empty main() and Ninety_Nine_Bottles_of_Beer()

---
+++ BearSong.c
@@ -0,0 +1,22 @@
+//
+//  BeerSong.c
+//  BeerSonG
+//
+//  Created by Zachary Denny on 9/25/18.
+//  Copyright © 2018 Zachary Denny. All rights reserved.
+//
+
+#include <stdio.h>
+
+//Main function
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
+
+//Ninety_Nine_Bottles_of_Beer() function
+void Ninety_Nine_Bottles_of_Beer(){
+    
+}
